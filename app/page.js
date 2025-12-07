64d953aa9f58e6942a0c8bb7b43a888380d8c426
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-12-08 01:59:00 +0530

    test

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import InviteSender from "@/components/InviteSender";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50">
+      <InviteSender />
+    </main>
+  );
+}
